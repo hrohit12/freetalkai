761b7b3f2d74322b9a3b552f5f70678bbb0adac3
hrohit12 <kamblehrohit12@gmail.com>
2026-05-05 19:25:59 +0530

    Add Monetag script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script src="https://5gvci.com/act/files/tag.min.js?z=10967483" data-cfasync="false" async></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
